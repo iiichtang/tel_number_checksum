7af62f745c82160c73105005c499dc16323bbb15
iiichtang <iiichtang@gmail.com>
2017-05-18 17:50:04 +0800

    add rules for tel validation

--- tel_validate.py
+++ tel_validate.py
@@ -1,8 +1,9 @@
 from country_call_code import *
 from area_code import *
+from tel_list_for_test import *
 
 CELL_PHONE_DICT = {'13': 1, '17': 1, '18': 1, '14': 1, '15': 1}
-HOTLINE_FIVE_DIGIT_DICT = {'12': 1, '95': 1}
+HOTLINE_FIVE_DIGIT_DICT = {'10': 1, '11': 1, '12': 1}
 HOTLINE_TEN_DIGIT_DICT = {'400': 1, '800': 1}
 TELECOM_DICT = {'10010': 1, '10000': 1, '10001': 1, '10086': 1}
 
@@ -16,9 +17,14 @@ def tel_validate(tel_string):
         # print("cell phone")
         return True
 
-    # 95, 12 Hotline
+    # 10, 11, 12 Hotline
     if tel_string[0:2] in HOTLINE_FIVE_DIGIT_DICT and len(tel_string) == 5:
         # print("95, 12 Hotline")
+        return True
+
+    # 95 Long tel
+    if tel_string.startswith("95") and 5 <= len(tel_string) <= 17:
+        # print("SMS")
         return True
 
     # 400, 800 Hotline
@@ -50,8 +56,18 @@ def tel_validate(tel_string):
     tel_without_area_code = remove_area_code(tel_string)
     if tel_without_area_code is not False:
         # 96 Hotline
-        if tel_without_area_code[:2] == "96" and 4 < len(tel_without_area_code) < 8:
+        if tel_without_area_code.startswith("96") and 5 <= len(tel_without_area_code) <= 7:
             # print("96 Hotline")
+            return True
+
+        # 10, 11, 12 Hotline
+        if tel_without_area_code[0:2] in HOTLINE_FIVE_DIGIT_DICT and len(tel_without_area_code) == 5:
+            # print("95, 12 Hotline")
+            return True
+
+        # 95 Long tel
+        if tel_without_area_code.startswith("95") and 5 <= len(tel_without_area_code) <= 17:
+            # print("SMS")
             return True
 
         # Normal tel
@@ -64,7 +80,6 @@ def tel_validate(tel_string):
 def remove_area_code(tel_string):
     area_code_length = 0
     if tel_string[:4] in AREA_FOUR_DIGIT_DICT:
-
         area_code_length = 4
     elif tel_string[:3] in AREA_THREE_DIGIT_DICT:
         area_code_length = 3
@@ -77,21 +92,28 @@ def remove_area_code(tel_string):
 
 def region_check(tel_string):
     # Check if + exist in the first digit
-    if not tel_string.startswith("+"):
+    if not tel_string.startswith("+") and not tel_string.startswith("00"):
         return False
 
+    # Remove Leading Symbol
+    tel_no_leading_symbol = ""
+    if tel_string.startswith("+"):
+        tel_no_leading_symbol = tel_string[1:]
+    elif tel_string.startswith("00"):
+        tel_no_leading_symbol = tel_string[2:]
+
     # Check if tel number contain any non int charactor
-    if not str(tel_string[1:]).isdigit():
+    if not str(tel_no_leading_symbol).isdigit():
         return False
 
     # In America
-    if tel_string.startswith("+1"):
+    if tel_no_leading_symbol.startswith("1"):
         # print("+1 region")
         return True
 
     # In China
-    if tel_string.startswith("+86"):
-        local_tel_without_region = tel_string[3:]
+    if tel_no_leading_symbol.startswith("86"):
+        local_tel_without_region = tel_no_leading_symbol[2:]
         tel_first_three = local_tel_without_region[:3]
         tel_first_two = local_tel_without_region[:2]
         if tel_first_three in AREA_FOUR_DIGIT_WITHOUT_ZERO_DICT or tel_first_two in AREA_THREE_DIGIT_WITHOUT_ZERO_DICT:
@@ -105,8 +127,8 @@ def region_check(tel_string):
         return False
 
     # In other country
-    tel_first_three = tel_string[:4]
-    tel_first_two = tel_string[:3]
+    tel_first_three = tel_no_leading_symbol[:3]
+    tel_first_two = tel_no_leading_symbol[:2]
 
     if tel_first_three in COUNTRY_CODE_DICT or tel_first_two in COUNTRY_CODE_DICT:
         # print("+xx region")
@@ -119,7 +141,7 @@ def tel_reform(tel_number):
     tel_string = str(tel_number).replace("-", "")
 
     # Region check
-    if tel_string[0] == "+":
+    if tel_string.startswith("+") or tel_string.startswith("00"):
         if region_check(tel_string):
             return tel_string
         return None
@@ -136,10 +158,9 @@ def tel_reform(tel_number):
 
 
 if __name__ == "__main__":
-    for tel in ["10690787100171"]:
+    for tel in valid_86_region_list:
         result = tel_reform(tel)
         if result is None:
-            print(tel)
+            print("%s => False" % tel)
         else:
             print("%s => %s" % (tel, tel_reform(tel)))
-
